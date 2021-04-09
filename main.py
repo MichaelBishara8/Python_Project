dcf1b958cc40a7cbbc2073c999f0e72029d9762c
michael <michaelbishara@protonmail.com>
2021-04-09 22:36:09 +1000

    Fixed some stuff, break into functions

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ words = ["python", "media", "computation", "science"]
 
 def main():
     word = list(rnd.choice(words))
-    hidden_word = ("_" * len(word))
+    hidden_word = list(("_" * len(word)))
     print("Welcome to HangMan! ಠ_ಠ")
     while hidden_word != word:
         print(hidden_word)
         char = getletter()
-        if char in word:
-            for i in word:
-                hidden_word[i] = char
+        for i in word:
+            if char in word:
+                hidden_word[word.index(i)] = char
+
+        else:
+            print("Nope, try again! XXX")
 
 
 def getletter():
     try:
         char = input("Enter a letter: ")
-        if char is not type(char):
+        if not char.isalpha() or len(char) != 1:
             raise TypeError("Error: A single character only")
         return char
     except TypeError:
         print("Test Error")
         getletter()
+
 
 main()
