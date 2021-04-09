73cdd3ec91f5a9741492408b832151039e9b36de
michael <michaelbishara@protonmail.com>
2021-04-09 22:43:47 +1000

    Game works

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ words = ["python", "media", "computation", "science"]
 def main():
     word = list(rnd.choice(words))
     hidden_word = list(("_" * len(word)))
+
     print("Welcome to HangMan! ಠ_ಠ")
+
     while hidden_word != word:
         print(hidden_word)
-        char = getletter()
-        for i in word:
-            if char in word:
-                hidden_word[word.index(i)] = char
+        char = get_letter()
 
+        if char in word:
+            for i in word:
+                if i == char:
+                    hidden_word[word.index(i)] = char
         else:
             print("Nope, try again! XXX")
 
 
-def getletter():
+def get_letter():
     try:
         char = input("Enter a letter: ")
         if not char.isalpha() or len(char) != 1:
@@ -26,7 +29,7 @@ def getletter():
         return char
     except TypeError:
         print("Test Error")
-        getletter()
+        get_letter()
 
 
 main()
