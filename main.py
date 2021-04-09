38c6cc7f1a92da89dbc6014a01187afc9c6f88e7
Michael <michaelbishara@protonmail.com>
2021-04-09 18:59:42 +1000

    whole lot of errors, break down into methods

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import random as rnd
 
 words = ["python", "media", "computation", "science"]
 
+
 def main():
-    word = rnd.choice(words)
+    word = list(rnd.choice(words))
+    hidden_word = ("_" * len(word))
     print("Welcome to HangMan! ಠ_ಠ")
-    print("_" * len(word))
+    while hidden_word != word:
+        print(hidden_word)
+        char = getletter()
+        if char in word:
+            for i in word:
+                hidden_word[i] = char
 
 
-if __name__ == "__main__":
-    main()
+def getletter():
+    try:
+        char = input("Enter a letter: ")
+        if char is not type(char):
+            raise TypeError("Error: A single character only")
+        return char
+    except TypeError:
+        print("Test Error")
+        getletter()
+
+main()
