6ac410da7bd838d2be94775b9971b255d0ebfdc6
Michael <michaelbishara@protonmail.com>
2021-04-10 23:24:14 +1000

    Checks for duplicate entries edited format added comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ words = ["python", "media", "computation", "science"]
 
 
 def main():
+    # Randomizes word choice from words list
     word = list(rnd.choice(words))
     hidden_word = list(("_" * len(word)))
 
@@ -12,33 +13,39 @@ def main():
     while hidden_word != word:
         print_list(hidden_word)
         char = get_letter()
-        if char in word:
+        # check for repeated words
+        if char in hidden_word:
+            print(char, "has already been entered!")
+            continue
+        # Entering the letter in the mystery word
+        elif char in word:
             for i, hidden_letter in enumerate(word):
                 if hidden_letter == char:
                     hidden_word[i] = char
         else:
-            print("Nope, try again! XXXabc")
-    print("Congrats!")
+            print("Nope, try again!")
+    print("Congrats! The word was:", ''.join(word).capitalize() + '!')
 
 
+# Tests the letter is alpha, a single character and makes it lowercase, otherwise it asks to re-enter
 def get_letter():
     try:
         char = input("Enter a letter: ")
         if not char.isalpha() or len(char) != 1:
             raise TypeError("Error: Incorrect input")
-        return char
+        return char.lower()
     except TypeError:
         print("Error: Only a single character - no numbers/symbols")
         get_letter()
 
-
+# Joins the word as a list into a string
 def print_list(word_list):
     print(''.join(word_list))
 
 
 main()
 
-
 # todo Check repeated letters
 # todo Scoreboard/players?
 # todo other exceptions to catch
+# todo Capital letters
