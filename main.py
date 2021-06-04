ee74d915b232ac5f9339478f67720a8570797a58
michael <michaelbishara@protonmail.com>
2021-06-04 14:07:08 +1000

    Added scoring and attempts removed print_list()

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random as rnd
 
 def main():
     words = ["python", "media", "computation", "science"]
+    score = 0
+    allowed_attempts = 5
     # Randomly chooses a word
     word = list(rnd.choice(words))
     # Word is hidden, underscores are a clue to how many letters
@@ -10,26 +12,33 @@ def main():
 
     print("Welcome to HangMan! ಠ_ಠ")
 
-    # Loop will continue till solved
-    while hidden_word != word:
-        print_list(hidden_word)
+    # Loop will continue till solved or 5 incorrect attempts
+    while hidden_word != word and allowed_attempts > 0:
+        print(''.join(hidden_word))
         char = get_letter()
         # check for repeated letters
         if char in hidden_word:
             print(char, "has already been entered!")
+            score -= 1
             continue
         # Updates hidden_word with correct char for all occurrences
         elif char in word:
             for i, hidden_letter in enumerate(word):
                 if hidden_letter == char:
                     hidden_word[i] = char
+                    score += 3
         else:
-            print("Nope, try again!")
-    print("Congrats! The word was:", ''.join(word).capitalize() + '!')
+            print("Nope, try again!\nYou have", allowed_attempts, "attempts remaining!")
+            score -= 1
+            allowed_attempts -= 1
+    if allowed_attempts > 1:
+        print("Congrats! The word was:", ''.join(word).capitalize() + '!')
+        print("You scored:", score)
+    else:
+        score -= 5
+        print("Bad luck! The word was:", ''.join(word).capitalize() + '!')
+        print("You scored:", score)
 
-
-# Tests the letter is alpha, a single character and makes it lowercase,
-# otherwise it asks to re-enter
 
 # Requests input and tests that it is alphabetical and singular then returns a lowercase char
 def get_letter():
@@ -41,11 +50,6 @@ def get_letter():
     except TypeError:
         print("Error: Only a single character - no numbers/symbols")
         get_letter()
-
-
-# Prints the list as a string
-def print_list(word_list):
-    print(''.join(word_list))
 
 
 if __name__ == "__main__":
